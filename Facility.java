f2856a72311474ad48437d64c74c662a861b0441
JackNarowski <jacknarowski@gmail.com>
2020-02-21 13:18:13 -0600

    More structure

--- Facility.java
+++ Facility.java
@@ -1,4 +1,11 @@
-interface Facility{
+import java.util.*;
 
+public class Facility {
+        public String type;
 
+        public void Type(String type) {
+                this.type = type;
         }
+
+
+}
---
+++ Inspection.java
@@ -0,0 +1,4 @@
+public class Inspection extends Facility {
+
+
+}
