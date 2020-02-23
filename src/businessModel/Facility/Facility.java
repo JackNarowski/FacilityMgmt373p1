cce3c2b9ba9094534c39d5ba6bdac404954d3bb4
alexandratcrane <acrane2@luc.edu>
2020-02-23 16:37:08 -0600

    Moved capacity from facility to use

--- src/businessModel/Facility/Facility.java
+++ src/businessModel/Facility/Facility.java
@@ -3,7 +3,6 @@ import java.util.*;
 public interface Facility {
         private String name;
         private Object info;
-        private Object capcaity;
         private Maintenance m;
         private Inspections i;
         private Use use;
