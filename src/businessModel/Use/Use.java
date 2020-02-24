77de0a9e1fb4b2f17757ff1dc09437a3f0d7e898
alexandratcrane <acrane2@luc.edu>
2020-02-23 20:10:03 -0600

    Added to Use.java

--- src/businessModel/Use/Use.java
+++ src/businessModel/Use/Use.java
@@ -4,6 +4,13 @@ public class Use {
 
     //Facility User
     private int capcaity; //how many people can be inside the facility at once
+    public Object UseSchedule;
+
+    public Object vacateFacility(){}
+    public Object calcUsageRate(){}
+    public Object requestAvailableCapacity(){}
+    public void addFacilityDetail(){}
+    public Object getFacilityInformation(){}
     //UseSchedule Subclass
 
 }
