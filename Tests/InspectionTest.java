07307516bf5e2a88f91cd63053d69662e7b25d1d
JackNarowski <jacknarowski@gmail.com>
2020-02-23 20:33:17 -0600

    UseTest functions added and initial other test files created

---
+++ Tests/InspectionTest.java
@@ -0,0 +1,2 @@
+public class InspectionTest {
+}
---
+++ Tests/UseTest.java
@@ -0,0 +1,30 @@
+public class UseTest {
+    public Object vacateFacilityTest(){
+        //SetUp
+        //vacateFacility();
+        //Test output
+    }
+    public Object calcUsageRateTest(){
+        //SetUp
+        //calcUsageRate();
+        //Test output
+
+    }
+    public Object requestAvailableCapacityTest(){
+        //Setup
+        //requestAvailableCapacity();
+        //Test output
+
+    }
+    public void addFacilityDetailTest() {
+        //Setup
+        //addFacilityDetail
+        //Test output
+    }
+    public Object getFacilityInformationTest() {
+        //Setup
+        //getFacilityInformation();
+        //Test output
+    }
+    }
+}
