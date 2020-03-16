c350c7eda97510916f8521f3c0f249f1ba28c785
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-03-15 21:18:43 -0500

    added things to Use class

--- src/businessModel/Use/Use.java
+++ src/businessModel/Use/Use.java
@@ -3,14 +3,23 @@ package businessModel.Use;
 public class Use {
 
     //Facility User
-    private int capcaity; //how many people can be inside the facility at once
+    public int capcaity; //how many people can be inside the facility at once
     public Object UseSchedule;
 
-    public Object vacateFacility(){}
-    public Object calcUsageRate(){}
-    public Object requestAvailableCapacity(){}
-    public void addFacilityDetail(){}
-    public Object getFacilityInformation(){}
+    private void vacateFacility(){capcaity = 0;}
+    public void setCapacity(int c) {capcaity = c;}
+    public Object calcUsageRate(){ 
+    	// TODO what kind of formula to make?
+    	
+    }
+    public int requestAvailableCapacity(){return capcaity;}
+    public void addFacilityDetail(){
+    	
+    	// TODO what kind of information to add in detail??
+    }
+    public Object getFacilityInformation(){
+    	// TODO might move this to the Facility class 
+    }
     //UseSchedule Subclass
 
 }
