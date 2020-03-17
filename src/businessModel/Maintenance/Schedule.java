903d850c8e801be8be8609359be391c7b1b049b1
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-03-16 21:32:17 -0500

    added to Schedule class. created a way to measure times. moved method from the Maitenance method

--- src/businessModel/Maintenance/Schedule.java
+++ src/businessModel/Maintenance/Schedule.java
@@ -7,4 +7,32 @@ public class Schedule implements Maintenance {
     public String getRequestName(){}
     public double timeStart(){}
     public double timeStop(){}
+    
+    
+    public Schedule(int startTime, int endTime) {
+    	if(validTime(startTime) & validTime(endTime)){}
+    	
+    
+    } //constructor 
+    	
+    
+    private boolean validTime(int time) {
+    	boolean scheduleConfirmed;
+    	
+    	if((time > 2359) & (time < 0)) {
+    		System.out.print("Time provided is out of bounds");
+    		 scheduleConfirmed = false;
+    	}//if  time is not a real time 
+    	else {
+    		scheduleConfirmed = true;
+    	}
+  
+    	
+    	
+    	return scheduleConfirmed;
+    	
+    }
+    
+    
+    
 }
