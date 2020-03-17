6005ee8950c2e758f4b63e7b4ddd72217acab7ec
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-03-16 21:34:53 -0500

    made changes to Schedule and Requests. im thinking of calling a schedule object in the request class

--- src/businessModel/Maintenance/Requests.java
+++ src/businessModel/Maintenance/Requests.java
@@ -1,8 +1,35 @@
 package businessModel.Maintenance;
 
 public class Requests implements Maintenance {
-    public Requests r;
+    public int id;
     public Schedule s;
 
-    public String getRequestName(){}
+    public void setRequestId(int id){
+    	this.id = id;
+    	
+    }//setId
+    
+    private int getRequestId() {
+    	return id;
+    	
+    }//getId
+    
+    public boolean setSchedule(int startTime, int endTime) {
+    	boolean scheduleConfirmed = false;
+    	
+    	if((startTime > 2359) & (startTime < 0)) {
+    		System.out.print("Time provided is out of bounds");
+    		return scheduleConfirmed = false;
+    	}//if start time is not a real time 
+    	if((endTime > 2359) & (endTime < 0)) {
+    		System.out.print("Time provided is out of bounds");
+    		return scheduleConfirmed = false;
+    	}//if end time not a real time
+    	
+    	
+    	
+    	return scheduleConfirmed;
+    	
+    }
+    
 }
