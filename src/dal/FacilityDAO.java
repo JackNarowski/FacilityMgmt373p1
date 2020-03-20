4df7d3946990506cecd75f27235be24ddd75e342
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-03-19 23:57:52 -0500

    Nevermind I asked my friend and she said you didn't have to do anything like that

--- src/dal/FacilityDAO.java
+++ src/dal/FacilityDAO.java
@@ -1,10 +1,10 @@
 package dal;
-import java.util.*;
 import businessModel.Facility.*;
 public interface FacilityDAO{
-	 public ArrayList<Facility> listFacilities();
+	 
 
 	 public void addNewFacility(Facility f);
 	 public void removeFacility(Facility f);
+	 public void getFacility(int id);
 	 
 }
