d1275c7b1c14f75c68734d6c760d8548f0717024
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-03-19 21:36:04 -0500

    Updated requests and schedule

--- src/businessModel/Maintenance/Schedule.java
+++ src/businessModel/Maintenance/Schedule.java
@@ -1,21 +1,28 @@
 package businessModel.Maintenance;
 
-public class Schedule implements Maintenance {
+public class Schedule{
     public Requests request;
     public Schedule schedule;
-    
+    public int startTime;
+    public int endTime;
     
     public Schedule(int startTime, int endTime) {
-    	if(validTime(startTime) & validTime(endTime)){} // TODO idk what we're gonna do here
-    	
+    	if(validTime(startTime) & validTime(endTime)){
+    		this.startTime = startTime;
+    		this.endTime = endTime;
+    		
+    	} // TODO idk what we're gonna do here
+    
     
     } //constructor 
     	
+    public int getStartTime() {return startTime;}
+    public int getEndTime() {return endTime;}
     
     private boolean validTime(int time) {
     	boolean scheduleConfirmed;
     	
-    	if((time > 2359) & (time < 0)) {
+    	if((time > 2359) | (time < 0)) {
     		System.out.print("Time provided is out of bounds");
     		 scheduleConfirmed = false;
     	}//if  time is not a real time 
