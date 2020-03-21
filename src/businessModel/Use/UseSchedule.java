38cdd660adbb90a4b86e637f663ccb9d420bd9b8
JackNarowski <jacknarowski@gmail.com>
2020-03-21 15:39:04 -0500

    Updated Use Schedule

--- src/businessModel/Use/UseSchedule.java
+++ src/businessModel/Use/UseSchedule.java
@@ -2,13 +2,32 @@ package businessModel.Use;
 
 import java.util.ArrayList;
 
-public class UseSchedule extends Use{
+public class UseSchedule{
+    public UseSchedule useschedule;
+    public int startTime;
+    public int endTime;
 
-    //useSchedule
-    public Object schedule;
+    public UseSchedule(int startTime, int endTime) {
+        if(validTime(startTime) & validTime(endTime)){
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+    } //constructor
 
-    public Object inUseDuringInterval(double interval){}
-    public Object assignFacilityToUse(Use use){}
-    public Object listActualUsage(){}
-    //inUseDuringInterval
+    public int getUseStartTime() {return startTime;}
+    public int getUseEndTime() {return endTime;}
+
+    private boolean validUseTime(int time) {
+        boolean scheduleConfirmed;
+
+        if((time > 2359) | (time < 0)) {
+            System.out.print("Time provided is out of bounds");
+            scheduleConfirmed = false;
+        }//if  time is not a real time
+        else {
+            scheduleConfirmed = true;
+        }
+        return scheduleConfirmed;
+
+    }
 }
