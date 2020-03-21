3991e46cc5e37671dcfc15460cadbadb66c70dcc
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-03-21 13:38:36 -0500

    made some fake DAL methods that won't be filled out

---
+++ src/dal/RequestsDAO.java
@@ -0,0 +1,9 @@
+package dal;
+
+import businessModel.Facility.Facility;
+
+public class RequestsDAO {
+	 public void addNewRequests(Facility f);
+	 public void removeRequests(Facility f);
+	 public void getRequests(int id);
+}
--- src/dal/ScheduleDAO.java
+++ src/dal/ScheduleDAO.java
@@ -1,11 +1,12 @@
 package dal;
+import businessModel.Facility.Facility;
 import businessModel.Use.*;
 import java.util.*;
 
 public class ScheduleDAO{
 
-    public void setEvent(Use u); //add SQL add
-    public void removeEvent(); //add SQL remove
-    public Use getSchedule(); // add SQL get
+	 public void addNewSchedule(Facility f);
+	 public void removeSchedule(Facility f);
+	 public void getSchedule(int id);
 
 }
