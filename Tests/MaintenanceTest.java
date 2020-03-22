ab4c1545c5b769f060a6d157a7c61995c5357cd8
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-03-22 13:31:06 -0500

    made changes to Maintenance tests

--- Tests/MaintenanceTest.java
+++ Tests/MaintenanceTest.java
@@ -1,48 +1,27 @@
-import businessModel.Maintenance.Maintenance;
-import businessModel.Maintenance.MaintenanceOrder;
-import businessModel.Maintenance.MaintenanceScheduler;
+import businessModel.Facility.Facility;
+import businessModel.Facility.FacilityFactory;
+import businessModel.Maintenance.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
 
 import java.util.ArrayList;
 
 public class MaintenanceTest {
-    public boolean makeFacilityMainRequestTest(){
+	@Test
+    void makeFacilityMainRequestTest(){
         //Setup
-        //makeFacilityMainRequest(Request r);
+
+    	Facility fac1 = FacilityFactory.addNewFacility(1);
+    	Requests req1 = new Requests(1);
+		fac1.getMaintManag().makeFacilityMaintRequest(req1);
+        assertEquals(1, req1.getRequestId());
+		//makeFacilityMainRequest(Request r);
         //Test output
     }
-    public boolean listMainRequestsTest() {
-        //Setup
-        //listMainRequests()
-        //Test output
-    }
-    public boolean scheduleMainenanceTest() {
-        //Setup
-        //scheduleMaitenance();
-        //Test output
-    }
-    public boolean getMaintenanceTest(){
-        //Setup
-        //getMaintenance();
-        //Test output
-    }
-    public boolean calcDownTimeForFacility() {
-        //Setup
-        //calcDownTimeForFacility();
-        //Test output
-    }
-    public boolean listMaintenance(){
-        //Setup
-        //listMaintenance();
-        //Test output
-    }
-    public boolean calcMaintenanceCostForFacility(){
-        //Setup
-        //calcMaitenanceCostForFacility();
-        //Test output
-    }
-    public boolean calcProblemRateForFacility(){
-        //Setup
-        //calcProblemRateForFacility();
-        //Test output
-    }
+   
 }
